f0f7994050fd8916b514d387310cc510ec4cea8f
Dan Bikle <danbikle@outlook.com>
2017-08-27 23:30:01 -0700

    more

--- tests/requestem.py
+++ tests/requestem.py
@@ -11,10 +11,15 @@ $PYTHON requestem.py
 import requests
 print('Busy...')
 
-def request_this(url_s):
-  response_r = requests.get(url_s)
+def request_this(url_s, params={'hl':2,'neurons':4}):
+  response_r = requests.get(url_s, params)
   if (response_r.status_code > 200):
     print(url_s,response_r.status_code)
+
+features_s = 'pct_lag1,slope3,dow,moy'
+params     = {'features':features_s, 'hl': 2,'neurons':4}
+
+# request_this("http://localhost:5011/keras_nn/FB/3/2017-08", params)
 
 request_this('http://localhost:5011/demos')
 request_this('http://localhost:5011/algo_demos')
@@ -26,6 +31,17 @@ request_this('http://localhost:5011/tkrs')
 request_this('http://localhost:5011/istkr/AAPL')
 request_this('http://localhost:5011/tkrprices/AAP')
 request_this('http://localhost:5011/dbtkrs')
+
+request_this("http://localhost:5011/sklinear/FB/3/2017-08/"+features_s)
+request_this("http://localhost:5011/sklinear_yr/FB/3/2017/"+features_s)
+request_this("http://localhost:5011/sklinear_tkr/FB/3/"+features_s)
+request_this("http://localhost:5011/keraslinear/FB/3/2017-08/"+features_s)
+request_this("http://localhost:5011/keraslinear_yr/FB/3/2017/"+features_s)
+request_this("http://localhost:5011/keraslinear_tkr/FB/3/"+features_s)
+
+request_this("http://localhost:5011/db/sklinear/FB/3/2017-08", params)
+request_this("http://localhost:5011/dbyr/sklinear/FB/3/2017" , params)
+request_this("http://localhost:5011/dbtkr/sklinear/FB/3"     , params)
 
 print('No news is probably good news.')
 print('Done')
