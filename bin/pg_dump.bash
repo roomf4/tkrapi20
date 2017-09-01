ca1f54657f17c7cfea0e595c98453816de2d15d5
Dan Bikle <danbikle@outlook.com>
2017-09-01 02:00:55 -0700

    more

--- bin/pg_dump.bash
+++ bin/pg_dump.bash
@@ -4,6 +4,8 @@
 
 # This script should create a pg-dump file.
 
-pg_dump --help
+# pg_dump --help
+
+PGPASSWORD=tkrapi pg_dump --no-owner -W -h 127.0.0.1 -d tkrapi -U tkrapi --no-tablespaces -t tkrprices --no-owner  -f /tmp/pgdump.sql
 
 exit
