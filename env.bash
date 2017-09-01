9655fb43163bd5149227c703b6e37c79c10a59e1
Dan Bikle <danbikle@outlook.com>
2017-08-31 19:21:04 -0700

    more

--- env.bash
+++ env.bash
@@ -18,5 +18,5 @@ export FLASK_DEBUG=1
 export PORT=5011
 export KERAS_BACKEND=tensorflow
 export PATH=${HOME}/anaconda3/bin:$PATH
-# for now:
+# app, tkrapi20 for now:
 export HPGURL=postgres://hpgygqtixbpwjf:70f594f2097cda735a7586cdd9b7133836d04fd6c5781c2a7314a957a94af2ab@ec2-54-163-233-201.compute-1.amazonaws.com:5432/d2nebmdp327bn5
