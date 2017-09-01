ebf757a961d56595f0d99a6ad1567acae0b7383c
Dan Bikle <danbikle@outlook.com>
2017-08-31 20:32:23 -0700

    more

--- env.bash
+++ env.bash
@@ -20,3 +20,5 @@ export KERAS_BACKEND=tensorflow
 export PATH=${HOME}/anaconda3/bin:$PATH
 # app, tkrapi20 for now:
 export HPGURL=postgres://hpgygqtixbpwjf:70f594f2097cda735a7586cdd9b7133836d04fd6c5781c2a7314a957a94af2ab@ec2-54-163-233-201.compute-1.amazonaws.com:5432/d2nebmdp327bn5
+# Note that I can assume the above variables do not exist in my heroku env.
+# There, however, I might need to set PYTHONPATH.
