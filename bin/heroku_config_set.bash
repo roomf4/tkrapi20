525747e0d6849054b773aefdc09a9d58801d5e2e
Dan Bikle <danbikle@outlook.com>
2017-09-02 14:46:33 -0700

    more

--- bin/heroku_config_set.bash
+++ bin/heroku_config_set.bash
@@ -4,8 +4,7 @@
 
 # This script should set env variables for my heroku env.
 
-heroku config:set  PGURL=postgres://hpgygqtixbpwjf:70f594f2097cda735a7586cdd9b7133836d04fd6c5781c2a7314a957a94af2ab@ec2-54-163-233-201.compute-1.amazonaws.com:5432/d2nebmdp327bn5
-heroku config:set HPGURL=postgres://hpgygqtixbpwjf:70f594f2097cda735a7586cdd9b7133836d04fd6c5781c2a7314a957a94af2ab@ec2-54-163-233-201.compute-1.amazonaws.com:5432/d2nebmdp327bn5
+heroku config:set DATABASE_URL=postgres://hpgygqtixbpwjf:70f594f2097cda735a7586cdd9b7133836d04fd6c5781c2a7314a957a94af2ab@ec2-54-163-233-201.compute-1.amazonaws.com:5432/d2nebmdp327bn5
 
 heroku config:set PYTHONPATH=/app/py
 
