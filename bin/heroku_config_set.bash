94c5083ad6c760146ad7194cd783234d7e8ff2bf
Dan Bikle <danbikle@outlook.com>
2017-09-01 23:45:13 -0700

    more

--- bin/heroku_config_set.bash
+++ bin/heroku_config_set.bash
@@ -6,4 +6,6 @@
 
 heroku config:set PGURL=postgres://hpgygqtixbpwjf:70f594f2097cda735a7586cdd9b7133836d04fd6c5781c2a7314a957a94af2ab@ec2-54-163-233-201.compute-1.amazonaws.com:5432/d2nebmdp327bn5
 
+heroku config:set PYTHONPATH=./py
+
 exit
