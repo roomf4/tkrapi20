3e24cc2900025a6cd6a5f749e82f4910feeb4aa9
Dan Bikle <danbikle@outlook.com>
2017-09-03 01:02:39 -0700

    more

--- wsgi.py
+++ wsgi.py
@@ -1,3 +1,8 @@
+"""
+wsgi.py
+This script should help me run flask-restful on heroku.
+I usually dont use this script on my laptop.
+"""
 from tflaskr import application
 
 if __name__ == "__main__":
