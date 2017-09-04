f10e6bab1c38fa7f74f47d4af5d847d0faa61874
Dan Bikle <danbikle@outlook.com>
2017-09-03 17:19:09 -0700

    more

--- tests/hcurlem.bash
+++ tests/hcurlem.bash
@@ -5,6 +5,13 @@
 # This script should use curl to run some simple tests against tkrapi20.herokuapp.com
 
 curl -v tkrapi20.herokuapp.com/demo11.json    > /tmp/htest10.txt
+curl -v "tkrapi20.herokuapp.com/csv/sklinear/FB/3/2017-08?features='pct_lag1,slope3,dow,moy'" > /tmp/htest50.txt
+curl -v "tkrapi20.herokuapp.com/csv/kerasnn/FB/3/2017-08?features='pct_lag1,slope4,moy'&hl=2&neurons=4" > /tmp/htest51.txt
+
+curl -v "tkrapi20.herokuapp.com/csvyr/sklinear/FB/3/2017?features='pct_lag1,slope3,dow,moy'"            > /tmp/htest52.txt
+curl -v "tkrapi20.herokuapp.com/csvtkr/sklinear/FB/3?features='pct_lag1,slope3,dow,moy'"                > /tmp/htest53.txt
+
+exit
 curl -v tkrapi20.herokuapp.com/features       > /tmp/htest13.txt
 curl -v tkrapi20.herokuapp.com/tkrinfo/AAPL   > /tmp/htest14.txt
 curl -v tkrapi20.herokuapp.com/tkrlist        > /tmp/htest15.txt
@@ -25,10 +32,5 @@ curl -v tkrapi20.herokuapp.com/kerasnn_dimensions    > /tmp/htest42.txt
 
 exit
 
-curl -v "tkrapi20.herokuapp.com/csv/sklinear/FB/3/2017-08?features='pct_lag1,slope3,dow,moy'" > /tmp/htest50.txt
-curl -v "tkrapi20.herokuapp.com/csv/kerasnn/FB/3/2017-08?features='pct_lag1,slope4,moy'&hl=2&neurons=4" > /tmp/htest51.txt
-
-curl -v "tkrapi20.herokuapp.com/csvyr/sklinear/FB/3/2017?features='pct_lag1,slope3,dow,moy'"            > /tmp/htest52.txt
-curl -v "tkrapi20.herokuapp.com/csvtkr/sklinear/FB/3?features='pct_lag1,slope3,dow,moy'"                > /tmp/htest53.txt
 
 
