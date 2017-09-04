b2c85862259a3c40255d997f024fe05b9459d458
Dan Bikle <danbikle@outlook.com>
2017-09-03 21:44:27 -0700

    more

--- py/dev.py
+++ py/dev.py
@@ -20,7 +20,17 @@ import sqlalchemy    as sql
 # from the py folder:
 import notf
 
+print(notf.tkrprices('SNAP'))
 print(notf.featuresCSV('SNAP')[:77])
+print(notf.getfeat('SNAP').tail())
+print(notf.tkrpricesCSV('SNAP')[:77])
+print(notf.getfeatures())
+print(notf.tkrinfo('SNAP'))
+print(notf.getmonths4tkr('FB',3))
+print(notf.dbpredictions_tkr())
+print(notf.prediction_counts())
+print(notf.prediction_dimensions())
+print(notf.kerasnn_dimensions())
 
 'bye'
 
