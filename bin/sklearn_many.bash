8cd0c60c0c088c499b6b43dd421dc9972d251400
Dan Bikle <danbikle@outlook.com>
2017-09-04 15:47:58 -0700

    more

--- bin/sklearn_many.bash
+++ bin/sklearn_many.bash
@@ -2,7 +2,7 @@
 
 # sklearn_many.bash
 
-# This script should learn predict a tkr over several dimensions.
+# This script should learn-predict a tkr over several dimensions.
 
 # The dimensions are:
 # features:
@@ -10,5 +10,29 @@
 #  , "slope3", "slope4", "slope5", "slope6", "slope7", "slope8", "slope9"
 #  , "dow", "moy"]
 # yrs: [3,4,5,7,10,15,20,25,30]
+
+# Demo:
+# sklearn_many.bash IBM
+
+if [ "$#" -ne 1 ]
+then
+    echo You should supply a Ticker.
+    echo Demo:
+    echo $0 IBM
+    exit 1
+fi
+
+SCRIPT=`realpath $0`
+SCRIPTPATH=`dirname $SCRIPT`
+cd ${SCRIPTPATH}/../
+. env.bash
+
+TKR=$1
+YRS=2017
+FEATURES='"pct_lag1", "pct_lag2", "pct_lag4"'
+
+echo busy with:
+echo $PYTHON py/sklearn_many.py $TKR $YRS $FEATURES
+$PYTHON py/sklearn_many.py $TKR $YRS $FEATURES
 
 exit
