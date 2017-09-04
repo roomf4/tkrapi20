462fc487cb5713a332f8170fcb57fd05531b9fc5
Dan Bikle <danbikle@outlook.com>
2017-09-04 15:20:16 -0700

    more

---
+++ bin/sklearn_many.bash
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# sklearn_many.bash
+
+# This script should learn predict a tkr over several dimensions.
+
+# The dimensions are:
+# features:
+# ["pct_lag1", "pct_lag2", "pct_lag4", "pct_lag8"
+#  , "slope3", "slope4", "slope5", "slope6", "slope7", "slope8", "slope9"
+#  , "dow", "moy"]
+# yrs: [3,4,5,7,10,15,20,25,30]
+
+exit
