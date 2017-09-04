12f111cbbb0fe96abc356251ff41c82ac9932ddb
Dan Bikle <danbikle@outlook.com>
2017-09-04 15:57:23 -0700

    more

---
+++ py/sklearn_many.py
@@ -0,0 +1,11 @@
+"""
+sklearn_many.py
+
+This script should learn-predict a tkr given some parameters.
+
+Demo:
+. env.bash
+$PYTHON py/sklearn_many.py FB 3 '["pct_lag1", "pct_lag2", "pct_lag4"]'
+"""
+
+
