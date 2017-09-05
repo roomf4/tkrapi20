57424f48d9663bd93917927b01641a91684443eb
Dan Bikle <danbikle@outlook.com>
2017-09-05 14:33:22 -0700

    more

--- bin/psql.bash
+++ bin/psql.bash
@@ -3,10 +3,14 @@
 # psql.bash
 
 # This script starts psql for me and connects me to correct host, role and db.
-. env.bash
 
+SCRIPT=`realpath $0`
+SCRIPTPATH=`dirname $SCRIPT`
+. ${SCRIPTPATH}/../env.bash
 # PGPASSWORD=tkrapi psql -aP pager=no -U tkrapi -h 127.0.0.1 tkrapi $@
-
+# Should be better syntax than above:
 psql -aP pager=no --dbname=$PGURL
+# Should be useful for look at data on Heroku:
+# psql -aP pager=no --dbname=$HPGURL
 
 exit
