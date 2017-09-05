e23b76ab064c08cf0a7460b1ef87a98eef0d94a0
Dan Bikle <danbikle@outlook.com>
2017-09-04 17:40:57 -0700

    more

--- py/sklearn_many.py
+++ py/sklearn_many.py
@@ -10,26 +10,21 @@ $PYTHON py/sklearn_many.py FB 3 '["pct_lag1", "pct_lag2", "pct_lag4"]'
 
 import pdb
 import sys
-pdb.set_trace()
 
 import sktkr
 
-if (len(sys.argv) != 2):
+if (len(sys.argv) < 4):
   print('I see a problem. Maybe you forgot a tkr?')
   print('Demo:')
-  print('~/anaconda3/bin/python '+sys.argv[0]+' IBM')
+  print('~/anaconda3/bin/python '+sys.argv[0]+' IBM 3 "pct_lag1", "pct_lag2", "pct_lag4" ')
   #  sys.exit(1)
   
 # I should get the tkr from the command line
-tkr = sys.argv[1]
-yrs_i = int(sys.argv[2])
-features_s = sys.argv[3:]
+tkr        =     sys.argv[1]
+yrs_i      = int(sys.argv[2])
+features_s = str(sys.argv[3:])
 #learn_predict_sklinear_tkr(tkr='ABC',yrs=20, features='pct_lag1,slope4,moy')
-sktkr.learn_predict_sklinear_tkr(
-    'FB'
-    ,3
-    ,features='"pct_lag1", "pct_lag2", "pct_lag4"'
-)
+sktkr.learn_predict_sklinear_tkr(tkr,yrs_i,features_s)
 
 'bye'
 
