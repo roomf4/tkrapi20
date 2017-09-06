44475ab47d7a6abdf225a807b4aee4da3645b158
Dan Bikle <danbikle@outlook.com>
2017-09-05 20:03:45 -0700

    more

--- py/sklearn_many.py
+++ py/sklearn_many.py
@@ -11,7 +11,7 @@ $PYTHON py/sklearn_many.py FB 3
 import datetime
 import pdb
 import sys
-
+# Should be in py folder:
 import notf
 import sktkr
 
@@ -21,20 +21,19 @@ if (len(sys.argv) < 2):
   print('~/anaconda3/bin/python '+sys.argv[0]+' IBM 3')
   #  sys.exit(1)
   
-# I should get the tkr from the command line
+# I should get the tkr,yrs from the command line:
 tkr   =     sys.argv[1]
 yrs_i = int(sys.argv[2])
+# I should declare various dimensions of predictions I want to generate:
 lags_s   = ',pct_lag2,pct_lag8'
 slopes_s = ',slope3,slope4' #,slope5,slope6,slope7,slope8,slope9'
 datef_s  = ',dow,moy'
-features_always_s    = 'pct_lag1,pct_lag4,'
+features_always_s    = 'pct_lag1,pct_lag4,' # Always use these to predict.
 features_sometimes_s = lags_s + slopes_s + datef_s
 
 # I should get all combinations of features in features_sometimes_s.
 features_l = sorted(features_sometimes_s.split(','))
 features_l.remove('')
-print(features_l)
-
 combos_l = notf.list2combos(features_l)
 
 # I should ask which month it is now:
@@ -45,9 +44,9 @@ for combo in combos_l:
   combo_s = ','.join(combo)
   features_s = features_always_s + combo_s
   print(features_s)
-  'sktkr.learn_predict_sklinear(tkr,yrs_i,mnth,features_s)'
+  sktkr.learn_predict_sklinear_tkr(tkr,yrs_i,features_s)
 # I should not forget this one:
-sktkr.learn_predict_sklinear(tkr,yrs_i,mnth,features_always_s)
+sktkr.learn_predict_sklinear_tkr(tkr,yrs_i,features_always_s)
 
 'bye'
 
