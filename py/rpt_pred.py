2e3aab71931987fdb7dd562ebb8f0762d94b7826
Dan Bikle <danbikle@outlook.com>
2017-09-06 16:28:31 -0700

    more

--- py/rpt_pred.py
+++ py/rpt_pred.py
@@ -15,24 +15,34 @@ import pandas        as pd
 import sqlalchemy    as sql
 # modules in the py folder:
 import pgdb
+pdb.set_trace()
 
-
-# I should loop through rows in the predictions table:
-db_s = os.environ['PGURL'] # s.b. local db not Heroku.
+# I should connect to local db, not Heroku:
+db_s = os.environ['PGURL']
 conn = sql.create_engine(db_s).connect()
-sql_s  = '''SELECT
+# I should drop the table I am about to fill:
+sql_s = 'DROP TABLE predictions2'
+conn.execute(sql_s)
+# I should loop through rows in the predictions table:
+sql_s = '''SELECT
 tkr,yrs,mnth,features,csv
 FROM predictions
 ORDER BY tkr,yrs,mnth,features
 LIMIT 22
 '''
-
 result = conn.execute(sql_s)
 if not result.rowcount:
   'return None'
 
 for row in result:
+  # I should access the CSV data in each row:
   pred_df = pd.read_csv(io.StringIO(row.csv))
+  # I should add columns which could be used as a concat-key:
+  pred_df['tkr']  = row.tkr
+  pred_df['yrs']  = row.yrs
+  pred_df['mnth'] = row.mnth
+  pred_df['features'] = row.features
+  pred_df.to_sql('predictions2', conn.engine, index=False, if_exists='append')
 'return True'
 
 'bye'
