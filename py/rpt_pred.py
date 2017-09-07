038a580fa0ef44f8fbea7ad01e01a8b2788affb7
Dan Bikle <danbikle@outlook.com>
2017-09-06 20:57:44 -0700

    more

--- py/rpt_pred.py
+++ py/rpt_pred.py
@@ -18,19 +18,22 @@ import sqlalchemy    as sql
 db_s = os.environ['PGURL']
 conn = sql.create_engine(db_s).connect()
 
-# I should query:
-sql_s = '''
-SELECT
-tkr
-,SUM(pct_lead) sum_pct_lead
-,MIN(cdate)    min_cdate
-,MAX(cdate)    max_cdate
-FROM predictions2
-GROUP BY tkr
-'''
-
-# I should copy result into DF:
-rpt_df = pd.read_sql(sql_s, conn)
-print(rpt_df)
+gbcol_l = ['tkr','yrs']
+for gbcol_s in gbcol_l:
+  # I should query:
+  sql_s = '''
+  SELECT
+  {}
+  ,SUM(pct_lead)      sum_pct_lead
+  ,SUM(effectiveness) sum_effectiveness
+  ,MIN(cdate)         min_cdate
+  ,MAX(cdate)         max_cdate
+  FROM predictions2
+  GROUP BY {}
+  '''.format(gbcol_s,gbcol_s)
+  print(sql_s)
+  # I should copy result into DF:
+  rpt_df = pd.read_sql(sql_s, conn)
+  print(rpt_df)
 
 'bye'
