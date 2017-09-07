d2f320f79a90d8697d5a130b20f863cb78d5a3df
Dan Bikle <danbikle@outlook.com>
2017-09-07 14:43:46 -0700

    more

--- py/dropout_study.py
+++ py/dropout_study.py
@@ -7,4 +7,56 @@ Demo:
 python dropout_study.py
 """
 
+import io
+import pdb
+import os
+import flask
+import datetime      as dt
+import flask_restful as fr
+import numpy         as np
+import pandas        as pd
+import sqlalchemy    as sql
+import keras
+# modules in the py folder:
+import pgdb
+
+# https://keras.io/models/model/#methods
+batch_size_i = 256 # Doc: Number of samples per gradient update.
+epochs_i     = 128 # Doc: Number of epochs to train the model.
+
+def learn_predict_keraslinear(tkr='FB',yrs=4,mnth='2017-08', features='pct_lag1,slope4,moy'):
+  """This function should use keras to learn, predict."""
+  features_s = pgdb.check_features(features)
+  # I should get train, test data.
+  # Also get copy of test data in a DataFrame for later reporting:
+  xtrain_a, ytrain_a, xtest_a, out_df = pgdb.get_train_test(tkr,yrs,mnth,features_s)
+  if ((xtrain_a.size == 0) or (ytrain_a.size == 0) or (xtest_a.size == 0)):
+    return out_df # probably empty too.
+  # Start using Keras here.
+  kmodel     = keras.models.Sequential()
+  # I should fit a Keras model to xtrain_a, ytrain_a
+  features_l = features_s.split(',')
+  features_i = len(features_l)
+  kmodel.add(keras.layers.core.Dense(features_i, input_shape=(features_i,)))
+  # https://keras.io/activations/
+  kmodel.add(keras.layers.core.Activation('linear'))
+  # I should have 1 linear-output:
+  kmodel.add(keras.layers.core.Dense(1)) 
+  kmodel.add(keras.layers.core.Activation('linear'))
+  kmodel.compile(loss='mean_squared_error', optimizer='adam')
+  kmodel.fit(xtrain_a,ytrain_a, batch_size=batch_size_i, epochs=epochs_i)
+  # I should predict xtest_a then update out_df
+  predictions_a           = np.round(kmodel.predict(xtest_a),3)
+  # Done with Keras, I should pass along the predictions.
+  predictions_l           = [p_f[0] for p_f in predictions_a] # I want a list
+  out_df['prediction']    = predictions_l
+  out_df['effectiveness'] = np.sign(out_df.pct_lead*out_df.prediction)*np.abs(out_df.pct_lead)
+  out_df['accuracy']      = (1+np.sign(out_df.effectiveness))/2
+  algo                    = 'keraslinear'
+  # I should return a DataFrame useful for reporting on the predictions.
+  return out_df
+
+out_df = learn_predict_keraslinear('FB','2017-08', 'pct_lag1,slope4,moy')
+print(out_df)
+
 'bye'
