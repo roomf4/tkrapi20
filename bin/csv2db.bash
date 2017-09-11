46e2e54ff90eac5ac6b9467b3a85c4e592e5b37e
Dan Bikle <danbikle@outlook.com>
2017-09-10 17:12:39 -0700

    more

---
+++ bin/csv2db.bash
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# csv2db.bash
+
+# This script should insert csv files into a table.
+# This script will hang if the FlaskRESTful server is running.
+# So I should shutdown the server before I run this script.
+
+SCRIPT=`realpath $0`
+SCRIPTPATH=`dirname $SCRIPT`
+cd ${SCRIPTPATH}/../
+
+. env.bash
+$PYTHON py/csv2db.py
+
+exit
