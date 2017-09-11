c6d2e5ac48746dea5c5736e79b896d60495b27b0
Dan Bikle <danbikle@outlook.com>
2017-09-10 21:38:19 -0700

    more

--- py/cr_models.py
+++ py/cr_models.py
@@ -8,14 +8,15 @@ import cr_model
 import pgdb
 
 tkr = '^GSPC'
-yrs = 30
 features_skinny_s = 'pct_lag1,slope3,moy'
 features_medium_s = 'pct_lag1,pct_lag4,slope3,slope6,moy'
 features_wide_s   = 'pct_lag1,pct_lag2,pct_lag4,pct_lag8,slope3,slope4,slope5,slope6,slope7,slope8,slope9,dow,moy'
 
-
-for mnth in pgdb.getmonths4tkr(tkr,yrs):
-  cr_model.cr_sklinear_model(tkr,yrs,mnth,features_skinny_s)
+for yrs in (10,20,30):
+  for features_s in (features_skinny_s,features_medium_s,features_wide_s):
+    print(tkr,yrs,features_s)
+    for mnth in pgdb.getmonths4tkr(tkr,yrs):
+      cr_model.cr_sklinear_model(tkr,yrs,mnth,features_s)
 
 
 'bye'
