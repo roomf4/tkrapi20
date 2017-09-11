9414b00535a4afaf2293b1102884223adfbe7f4f
Dan Bikle <danbikle@outlook.com>
2017-09-10 22:26:27 -0700

    more

---
+++ bin/conda_ins.bash
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# conda_ins.bash
+
+# This script should install some useful packages.
+
+conda install flask keras numpy pandas psycopg2 sqlalchemy
+conda install flask-restful -c conda-forge
+
+exit
