38376380764b8b01bf3d6a56c279c44d7180fe84
Dan Bikle <danbikle@outlook.com>
2017-09-10 17:56:43 -0700

    more

--- py/cr_model.py
+++ py/cr_model.py
@@ -2,10 +2,24 @@
 cr_model.py
 
 This script should create ML-models and save them to db table: mlmodels.
+
+Demo:
+. ../env.bash
+$PYTHON cr_model.py
 """
 
+import pdb
 # modules in the py folder:
-import sktkr
 import kerastkr
+import pgdb
+import sktkr
+
+def cr_sklinear_model(tkr='^GSPC', yrs=20, mnth='2017-08', features='pct_lag1,slope3,moy'):
+  """This function should use sklearn to create a model."""
+  features_s = pgdb.check_features(features)
+  return True
+
+pdb.set_trace()
+cr_sklinear_model()
 
 'bye'
