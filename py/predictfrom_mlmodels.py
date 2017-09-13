54c30697587ad50e92e4d663228bfb087e72a749
Dan Bikle <danbikle@outlook.com>
2017-09-13 15:01:01 -0700

    more

--- py/predictfrom_mlmodels.py
+++ py/predictfrom_mlmodels.py
@@ -10,11 +10,28 @@ predictfrom_mlmodels.py
 $PYTHON py/predictfrom_mlmodels.py
 """
 
+import io
 import pdb
+import os
+import datetime      as dt
+import numpy         as np
+import pandas        as pd
+import sqlalchemy    as sql
+import sklearn.linear_model as skl
+# modules in the py folder:
 import cr_model
 import pgdb
 
+# I should connect to the DB
+db_s = os.environ['DATABASE_URL']
+conn = sql.create_engine(db_s).connect()
+
 # I should use python to get a model from  mlmodels table.
+
+# select * from mlmodels order by crtime desc limit 1;
+
+sql_s = "select * from mlmodels order by crtime desc limit 1"
+
 # I should use python to get suitable test data from features table.
 
 
