aef9e10ade952e50f3300ee89deb51a380dafef1
Dan Bikle <danbikle@outlook.com>
2017-09-14 16:16:15 -0700

    more

--- js/js10.js
+++ js/js10.js
@@ -2,18 +2,17 @@
 
 // This script should demo nodejs.
 
-function print(inp) {
-    console.log(inp)
+function p(inp) {
+  console.log(inp)
 }
 
 debugger
 var hello = 123.4
 hello = 123.45
-print(hello)
+p(hello)
 
 my_a = [1.1,2.2,3.2,4.4,5.1,6.2,7.7,8.3,9.9]
-
-print(my_a)
+p(my_a)
 
 var timeseries = require("timeseries-analysis");
 
