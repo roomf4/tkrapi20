145cbfb7e15d2d9b4ccb4badc7f4ca287955f53f
Dan Bikle <danbikle@outlook.com>
2017-09-13 23:01:58 -0700

    more

--- py/predictfrom_mlmodels.py
+++ py/predictfrom_mlmodels.py
@@ -21,16 +21,29 @@ import sklearn.linear_model as skl
 # modules in the py folder:
 import cr_model
 import pgdb
+import sktkr
 
 # I should connect to the DB
 db_s = os.environ['DATABASE_URL']
 conn = sql.create_engine(db_s).connect()
 
+features_skinny_s = 'pct_lag1,slope3,moy'
+features_medium_s = 'pct_lag1,pct_lag4,slope3,slope6,moy'
+
+# I should use older syntax to ensure I have a model for this script:
+tkr  = '^GSPC'
+yrs  = 25
+mnth = '2017-08'
+features_s = features_medium_s
+cr_model.cr_sklinear_model(tkr,yrs,mnth,features_s)
+
 # I should use python to get a model from  mlmodels table.
 
 # select * from mlmodels order by crtime desc limit 1;
-
-sql_s  = "select * from mlmodels order by crtime desc limit 1"
+sql_s  = '''select
+tkr,yrs,mnth,features,sklinear_coef
+from mlmodels order by crtime desc limit 1
+'''
 
 result = conn.execute(sql_s)
 
@@ -49,26 +62,26 @@ print(feat_df.tail())
 
 # I should extract a numpy array from feat_df
 
-print(tkr,5,mnth)
-print(pgdb.check_features(features))
 xtrain_a, ytrain_a, xtest_a, out_df = pgdb.get_train_test(tkr,5,mnth,pgdb.check_features(features))
-print(out_df)
-print(xtest_a)
 
 # I should transform coef_s into array:
-print(coef_s)
-pdb.set_trace()
 coef_l = coef_s.split(',')
 coef_a = np.array(coef_l).reshape(len(coef_l),-1).astype(float)
-print(coef_a)
 
 # I should calc predictions using matmul:
 
 # I should add a column of 1s to xtest_a
-a1_a  = np.ones((len(xtest_a),1), dtype=np.float)
+a1_a   = np.ones((len(xtest_a),1), dtype=np.float)
 test_a = np.concatenate((a1_a,xtest_a), axis=1)# horizontally
 predictions_a = np.matmul(test_a,coef_a)
+print('predictions_a from matmul:')
+print(predictions_a)
 
+# I should use sklearn too:
+
+out_df = sktkr.learn_predict_sklinear(tkr,yrs,mnth,features)
+pdb.set_trace()
+print(out_df.prediction)
 
 'bye'
 
