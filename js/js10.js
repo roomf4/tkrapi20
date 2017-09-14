f639ac9c9a804fad92823114fae70a9ece91b6e2
Dan Bikle <danbikle@outlook.com>
2017-09-14 16:14:30 -0700

    more

--- js/js10.js
+++ js/js10.js
@@ -6,8 +6,15 @@ function print(inp) {
     console.log(inp)
 }
 
+debugger
 var hello = 123.4
-
+hello = 123.45
 print(hello)
+
+my_a = [1.1,2.2,3.2,4.4,5.1,6.2,7.7,8.3,9.9]
+
+print(my_a)
+
+var timeseries = require("timeseries-analysis");
 
 'bye'
