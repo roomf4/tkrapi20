f6720b4901e61075ebb32d24fbe9d16ba1418f94
Dan Bikle <danbikle@outlook.com>
2017-09-14 22:18:55 -0700

    more

--- js/static/my.js
+++ js/static/my.js
@@ -3,4 +3,18 @@
 
 my_a = [1.1,2.2,3.3]
 
+function d3csv_callback(arg0) {
+    arg0
+    true
+}
+
+function d3csv_rowparse(row) {
+    row
+    row.Date
+    row.Close
+    [row.Date, row.Close]
+}
+
+d3.csv('/static/^GSPC.csv',d3csv_rowparse,d3csv_callback)
+
 'bye'
