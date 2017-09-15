cb39659f5ed9ec4a515b6bfa49f15c41eaf1a9c8
Dan Bikle <danbikle@outlook.com>
2017-09-15 12:36:20 -0700

    more

---
+++ py/feat2file.py
@@ -0,0 +1,15 @@
+"""
+feat2file.py
+
+This function should write a CSV-file full of features for a tkr.
+Demo:
+. env.bash
+python py/feat2file.py
+"""
+
+import pgdb
+
+pgdb.getfeat2file('FB')
+print("pgdb.getfeat2file('FB') done.")
+
+'bye'
