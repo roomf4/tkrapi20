ba263a7f21e283e963dbccd062c5cfbe398f07a9
Dan Bikle <danbikle@outlook.com>
2017-09-14 22:54:17 -0700

    more

--- js/static/my.js
+++ js/static/my.js
@@ -1,17 +1,13 @@
-// my.js
-// This script should copy CSV data into an array and then create an array full of features.
+/* my.js
+This script should copy CSV data into an array and then create an array full of features.
+*/
 
-my_a = [1.1,2.2,3.3]
-
-function d3csv_callback(arg0) {
-    arg0
-    true
+function d3csv_callback(csv_a) {
+    // This function should expose CSV data from d3.csv() via parameter: csv_a.
+    csv_a
 }
 
 function d3csv_rowparse(row) {
-    row
-    row.Date
-    row.Close
     return [row.Date, +row.Close]
 }
 
