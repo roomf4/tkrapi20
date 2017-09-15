6841a7d0fa78578c3df6b23fec689010441b0a39
Dan Bikle <danbikle@outlook.com>
2017-09-14 18:47:45 -0700

    more

---
+++ js/flask4js.py
@@ -0,0 +1,21 @@
+"""
+flask4js.py
+
+This script should run a flask server which serves static HTML and JS.
+
+Demo:
+echo hello > static/my.html
+export FLASK_DEBUG=1
+python flask4js.py
+curl 127.0.0.1:5000/static/my.html
+"""
+
+import os
+from   flask import Flask
+application = Flask(__name__)
+                               
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    application.run(host='0.0.0.0', port=port)
+'bye'
+
