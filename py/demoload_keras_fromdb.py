80120a41d804025d1cdf6adee6522508241a2b77
Dan Bikle <danbikle@outlook.com>
2017-11-16 18:15:05 -0800

    more

--- py/demoload_keras_fromdb.py
+++ py/demoload_keras_fromdb.py
@@ -1,7 +1,13 @@
 """
 demoload_keras_fromdb.py
 
-This script should demonstrate how to load a keras model from db and use it to predict.
+This script should demonstrate how to 
+use Keras how to learn and predict by calling:
+kerastkr.learn_predict_keraslinear()
+And it shows how to load a model into a DB row by calling:
+pgdb.predictions2db()
+And it it shows how to feed a model from a DB row into keras.models.load_model()
+Finally, it shows how to generate predictions from xtest_a fed to that model.
 
 Demo:
 . ../env.bash
@@ -12,8 +18,12 @@ Demo:
 import kerastkr
 import pdb
 
-pdb.set_trace()
-out_df = kerastkr.load_predict_keraslinear()
+# pdb.set_trace()
+out_df = kerastkr.load_predict_keraslinear(tkr='FB'
+                                           ,yrs=4
+                                           ,mnth='2017-10'
+                                           ,features='pct_lag1,slope4,moy')
+# I should see a DF with about 21 rows:
 print(out_df)
 
 'bye'
