095c70b18def1a7f23509b44fedbd24b0873991f
Dan Bikle <danbikle@outlook.com>
2017-11-17 15:15:51 -0800

    more

---
+++ demo/csv2db.bash
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# ~/tkrapi20/demo/csv2db.bash
+
+# This script demonstrates how to read a CSV file into a Python string.
+
+# Then I show how to load that string into a text datatype inside a row inside Postgres.
+
+cd ~/tkrapi20/
+bin/csv2db.bash
+
+exit
